0fe13129f74f05cd13d20dbc78468958c7d88ada
xhacker-zzz <959220793@qq.com>
2023-01-20 20:27:36 +0800

    use wasm in npm packages

--- src/decrypt/kgm_wasm.ts
+++ src/decrypt/kgm_wasm.ts
@@ -1,4 +1,5 @@
-import KgmCryptoModule from '@/KgmWasm/KgmWasmBundle';
+import { KgmCrypto } from '@xhacker/kgmwasm/KgmWasmBundle';
+import KgmCryptoModule from '@xhacker/kgmwasm/KgmWasmBundle';
 import { MergeUint8Array } from '@/utils/MergeUint8Array';
 
 // 每次处理 2M 的数据
@@ -20,26 +21,26 @@ export async function DecryptKgmWasm(kgmBlob: ArrayBuffer, ext: string): Promise
   const result: KGMDecryptionResult = { success: false, data: new Uint8Array(), error: '' };
 
   // 初始化模组
-  let KgmCrypto: any;
+  let KgmCryptoObj: KgmCrypto;
 
   try {
-    KgmCrypto = await KgmCryptoModule();
+    KgmCryptoObj = await KgmCryptoModule();
   } catch (err: any) {
     result.error = err?.message || 'wasm 加载失败';
     return result;
   }
-  if (!KgmCrypto) {
+  if (!KgmCryptoObj) {
     result.error = 'wasm 加载失败';
     return result;
   }
 
   // 申请内存块，并文件末端数据到 WASM 的内存堆
   let kgmBuf = new Uint8Array(kgmBlob);
-  const pQmcBuf = KgmCrypto._malloc(DECRYPTION_BUF_SIZE);
-  KgmCrypto.writeArrayToMemory(kgmBuf.slice(0, DECRYPTION_BUF_SIZE), pQmcBuf);
+  const pQmcBuf = KgmCryptoObj._malloc(DECRYPTION_BUF_SIZE);
+  KgmCryptoObj.writeArrayToMemory(kgmBuf.slice(0, DECRYPTION_BUF_SIZE), pQmcBuf);
 
   // 进行解密初始化
-  const headerSize = KgmCrypto.preDec(pQmcBuf, DECRYPTION_BUF_SIZE, ext);
+  const headerSize = KgmCryptoObj.preDec(pQmcBuf, DECRYPTION_BUF_SIZE, ext);
   console.log(headerSize);
   kgmBuf = kgmBuf.slice(headerSize);
 
@@ -51,14 +52,14 @@ export async function DecryptKgmWasm(kgmBlob: ArrayBuffer, ext: string): Promise
 
     // 解密一些片段
     const blockData = new Uint8Array(kgmBuf.slice(offset, offset + blockSize));
-    KgmCrypto.writeArrayToMemory(blockData, pQmcBuf);
-    KgmCrypto.decBlob(pQmcBuf, blockSize, offset);
-    decryptedParts.push(KgmCrypto.HEAPU8.slice(pQmcBuf, pQmcBuf + blockSize));
+    KgmCryptoObj.writeArrayToMemory(blockData, pQmcBuf);
+    KgmCryptoObj.decBlob(pQmcBuf, blockSize, offset);
+    decryptedParts.push(KgmCryptoObj.HEAPU8.slice(pQmcBuf, pQmcBuf + blockSize));
 
     offset += blockSize;
     bytesToDecrypt -= blockSize;
   }
-  KgmCrypto._free(pQmcBuf);
+  KgmCryptoObj._free(pQmcBuf);
 
   result.data = MergeUint8Array(decryptedParts);
   result.success = true;
